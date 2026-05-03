16c6fc0bfd3030bd420b360675ed13fe4e98fcd7
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-05-04 01:39:45 +0530

    fix

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const authRoutes = require("./routes/auth.routes");
+
+
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-
+app.use("/auth", authRoutes);
 // Connect to DB
 connectDB();
 
